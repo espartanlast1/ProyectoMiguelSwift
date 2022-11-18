5852c77eecdee04e5ba044682f1de5351a749c9f
jorge <jorgecastilla@outlook.com>
2022-11-18 23:29:34 +0100

    Codigo Miguel

--- ProyectoMiguel/main.swift
+++ ProyectoMiguel/main.swift
@@ -8,4 +8,28 @@
 import Foundation
 
 print("Hello, World!")
+var numero = Int.random(in: 1..<100)
+var num = 0
+var i = 0
+
+while true{
+    print("¿Que numero piensas que es?")
+    var read = readLine()
+    num = (read! as NSString).integerValue
+    if num > numero {
+        i=i+1
+        print("Tu numero es demasiado grande")
+    } else if num < numero {
+        i=i+1
+        print("Tu numero es demasiado pequeño")
+    } else {
+        i=i+1
+        print("Adivinaste el numero, ENHORABUENA!!")
+        print("Has necesitado \(i) intentos para adivinar el numero")
+        break
+    }
+    
+}
+
+
 
